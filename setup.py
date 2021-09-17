0d5791f7875c036d743953d4abdedff3689d63c7
vwrj <8609620+vwrj@users.noreply.github.com>
2021-09-17 10:06:51 -0700

    bump version (#20)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.11",
+    version="0.7.12",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
