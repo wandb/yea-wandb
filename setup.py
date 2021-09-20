f0117c2ba253b28cc8bceb90092c1eb67408755e
Jeff Raubitschek <jeff@raubitschek.org>
2021-09-20 06:34:09 -0700

    bump 0.7.14

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.13",
+    version="0.7.14",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
