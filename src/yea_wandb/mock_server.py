09d8a13c9b426767456067a03b16002e55e1fcfa
Danny Goldstein <danny@goldstein.cloud>
2021-10-02 10:52:00 -0700

    add explicit ack path introspection to mock server (#22)

--- src/yea_wandb/mock_server.py
+++ src/yea_wandb/mock_server.py
@@ -565,6 +565,7 @@ def create_app(user_ctx=None):
                         "outOfDate": ctx.get("out_of_date", False),
                         "latestVersionString": str(ctx.get("latest_version", "0.9.42")),
                     },
+                    "exposesExplicitRunQueueAckPath": True,
                 }
             }
 
