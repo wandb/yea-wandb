637b80809ec80805c43ebaad48190ea6fba9c845
Danny Goldstein <danny@goldstein.cloud>
2021-10-02 11:22:42 -0700

    Put `exposesExplicitRunQueueAckPath` in the correct place and bump version (#24)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.16",
+    version="0.7.17",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
