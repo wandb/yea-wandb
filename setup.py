2de525fb89d719ad4e9df6766ed29dbd59639b4d
Jeff Raubitschek <jeff@wandb.com>
2021-10-14 10:45:04 -0700

    0.7.20

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.19",
+    version="0.7.20",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
