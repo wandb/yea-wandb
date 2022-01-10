af2023656e9b8e14a858cb2f57583b0cf64b9dad
Dmitry Duev <dmitry.duev@gmail.com>
2022-01-10 13:29:42 -0800

    make code_saving_enabled configurable in mock_server.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.28",
+    version="0.7.29",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
