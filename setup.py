e1c013bb7da34fa984db8b5f1fc502ef943d7e72
Dmitry Duev <dmitry.duev@gmail.com>
2022-01-20 11:25:30 -0800

    version++ + pin yea

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.29",
+    version="0.7.30",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
