3d1f64182fd2ed9245666c2d0dc511b445123398
Jeff Raubitschek <jeff@wandb.com>
2022-01-26 07:18:16 -0800

    Add backward compatibility

--- src/yea_wandb/artifact_emu.py
+++ src/yea_wandb/artifact_emu.py
@@ -92,8 +92,10 @@ class ArtifactEmulator:
         art_name = variables.get("name")
         assert art_id or art_name
 
-        is_public_api_query = query and (query.startswith(public_api_query_str)
-                                         or query.startswith(public_api_query_str2))
+        is_public_api_query = query and (
+            query.startswith(public_api_query_str)
+            or query.startswith(public_api_query_str2)
+        )
 
         if art_name:
             collection_name, version = art_name.split(":", 1)
