5279fbae846ec1786e1877a97cbeda436f5ef172
Dmitry Duev <dmitryduev@users.noreply.github.com>
2022-02-10 11:35:32 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.33",
+    version="0.7.34",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
