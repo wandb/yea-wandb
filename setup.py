ba5f10fe11c8dbc228669f9fd62dd6f82491fc73
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2022-03-03 02:18:30 -0800

    Expose history from backend (#49)
    * expose history

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.42",
+    version="0.7.43",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
