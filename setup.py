af6cd409254e4cee53f46f4e38c0c6f8828fdaa3
Vishwaesh Rajiv <vrajiv@vishwaeshs-mbp.lan>
2022-03-08 16:36:57 -0800

    bump final

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.45-dev",
+    version="0.7.45",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
--- src/yea_wandb/__init__.py
+++ src/yea_wandb/__init__.py
@@ -2,4 +2,4 @@ from .setup import setup
 
 
 __all__ = ["setup"]
-__version__ = "0.7.45-dev"
+__version__ = "0.7.45"
