33bfded2185e232415df1601b5c6ffc1dc2ddf7f
Dmitry Duev <dmitryduev@users.noreply.github.com>
2022-05-11 09:25:55 -0700

    Make remote base url configurable in relay mode (#61)
    * Make remote base url configurable in relay mode

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.52",
+    version="0.7.53-dev",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
