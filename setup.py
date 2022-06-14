9dc360ce6b7ca64e2b7b1bc8846879841c2bfbea
KyleGoyette <kdgoyette@gmail.com>
2022-06-14 10:25:12 -0700

    Add UpdateArtifact mutation, and version index to artifact emulator (#67)
    * fix
    
    * remove fix
    
    * bump to dev
    
    * bump from dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.56",
+    version="0.7.57",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
