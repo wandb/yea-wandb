5fd77a6832952db17e39829de9f4aaef278fc222
KyleGoyette <kdgoyette@gmail.com>
2022-06-14 11:03:05 -0700

    Update return for GetRunQueueItem (#68)
    * update returned shape
    
    * bump to dev
    
    * bump from dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.7.57",
+    version="0.7.58",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
