abf5c1e72641c5ed3f98670879990de2944c0514
Katia <katia@wandb.com>
2022-08-08 14:17:19 -0700

    bumpversion

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.8.7",
+    version="0.8.8",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
