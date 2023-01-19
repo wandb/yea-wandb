9a8c669ee41bd6db67f8dbac13e8ca2104c2d21a
Ishita Bindlish <ishita.bindlish@wandb.com>
2023-01-19 10:29:31 -0800

    bumpversion-to-dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.9.0",
+    version="0.9.1-dev",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
