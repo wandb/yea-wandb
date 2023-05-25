cecb23ea56bba34b6bea9b988774a3a79bad306f
Szymon Piechowicz <127154459+szymon-piechowicz-wandb@users.noreply.github.com>
2023-05-25 20:32:25 +0200

    Sync from wandb PR #5534 (#111)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.9.8",
+    version="0.9.9",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
