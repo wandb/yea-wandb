62fcc1164e389ccd2760ec34e5820bda4a178780
Estella Xin <estella.xin@wandb.com>
2023-08-11 17:31:56 -0400

    from dev

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.9.17-dev",
+    version="0.9.17",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
