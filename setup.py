20b51a634a6146b0af4ea065519cfd35e7c6e9e1
Katia Patkin <87335417+kptkin@users.noreply.github.com>
2023-09-19 13:23:31 -0700

    chore: bump version for release (#127)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="yea-wandb",
-    version="0.9.19",
+    version="0.9.20",
     description="Test harness wandb plugin",
     packages=["yea_wandb"],
     install_requires=[
